ae40a847cf993e995a754c880a2262714f311a48
mgazanayi <marouane.gazanayi@gmail.com>
2015-03-21 00:04:30 +0100

    Include barcode scanner
                                |\ /| /|_/|
                              |\||-|\||-/|/|
                               \\|\|//||///
              _..----.._       |\/\||//||||
            .'     o    '.     |||\\|/\\ ||
           /   o       o  \    | './\_/.' |
          |o        o     o|   |          |
          /'-.._o     __.-'\   |          |
          \      `````     /   |          |
          |``--........--'`|    '.______.'
           \              /
            `'----------'`

--- app/src/main/java/foodalert/food_alert/MainActivity.java
+++ app/src/main/java/foodalert/food_alert/MainActivity.java
@@ -5,37 +5,55 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.google.zxing.Result;
 
-public class MainActivity extends ActionBarActivity {
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+public class MainActivity extends ActionBarActivity implements ZXingScannerView.ResultHandler {
+
+    private ZXingScannerView scannerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        scannerView = new ZXingScannerView(this);
+        setContentView(scannerView);
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             startActivity(new Intent(this, SettingsActivity.class));
             return true;
         }
-
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        scannerView.setResultHandler(this);
+        scannerView.startCamera();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        scannerView.stopCamera();
+    }
+
+    @Override
+    public void handleResult(Result result) {
+        Toast.makeText(this, result.getText(), Toast.LENGTH_LONG).show();
     }
 }
